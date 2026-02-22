5f51ebff7861d53b343e908c8a5682887c6d8d8d
starlix234 <102003671+starlix234@users.noreply.github.com>
2026-02-22 04:12:43 -0300

    Add files via upload

---
+++ uninstall.php
@@ -0,0 +1,11 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+global $wpdb;
+$like = '%newstler_%';
+$wpdb->query( $wpdb->prepare(
+    "DELETE FROM $wpdb->options WHERE option_name LIKE %s OR option_name LIKE %s",
+    $like, '_transient_' . $like
+) );
